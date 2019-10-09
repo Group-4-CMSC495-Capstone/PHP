171656ef941cac4ec8907df35d28eaed0bfe6a8a
Dillan Cobb <piysta@yahoo.com>
2019-10-08 21:25:35 -0500

    Update index.php
    added comments and mysqli closes

--- index.php
+++ index.php
@@ -6,11 +6,15 @@ and open the template in the editor.
 -->
 
 <?php 
-	
+	// Starts session
 	session_start();
 
+	// Variable for error message handling
 	$error = "";
 
+	/* Checks if the 'logout' is inside of the get, if so - logs out of the current account
+	if the id exists, then proceed to the logged in page.
+	*/
 	if (array_key_exists("logout", $_GET)) {
 		unset($_SESSION);
 		//setcookie("id", "", time()-60*60);
@@ -19,25 +23,35 @@ and open the template in the editor.
 		header("Location: events.php");
 	}
 
+	// Once a user clicks the submit button.
 	if (array_key_exists("submit", $_POST)) {
 		
-		$link = mysqli_connect("localhost", "admin", "xxxxxxxx", "Emailer");
+		// DB connection
+		$link = mysqli_connect("localhost", "admin", "xxxxxx", "Emailer");
 
+		// Error handling if the DB isn't reached.
 		if (mysqli_connect_error()) {
 			die ("Database Connection Error<br>");
 		}
 
+		// Error message if there was no username in the POST
 		if (!$_POST['username']) {
 			$error .= "A username is required.<br>";
 		}
 
+		// Error message if there was no password in the POST
 		if (!$_POST['password']) {
 			$error .= "A password is required.<br>";
 		}
 
+		/*
+		If there is a error message, display error, else proceed with accordance to POST 			contents
+		*/
 		if ($error != "") {
 			$error = "<p>There were error(s) on the page:</p>".$error;
 		} else {
+
+			// Runs if there is a post register of 1. Only for account creation
 			if ($_POST['register'] == '1') {
 
 				if (!$_POST['email']) {
@@ -47,11 +61,11 @@ and open the template in the editor.
 
 					$result = mysqli_query($link, $query);
 
+					// If there is a result, there is an account. If none, insert new account
 					if (mysqli_num_rows($result) > 0) {
 						$error = "That username is taken.";
 					} else {
 						$query = "INSERT INTO Accounts (username, password, email) VALUES ('".mysqli_real_escape_string($link, $_POST['username'])."', '".mysqli_real_escape_string($link, $_POST['password'])."', '".mysqli_real_escape_string($link, $_POST['email'])."');";
-
 
 						if (!mysqli_query($link, $query)) {
 							echo "<p>Could not create an account.</p>";
@@ -70,12 +84,15 @@ and open the template in the editor.
 				}
 				
 			} else {
+
+				// No register - FOR LOGGING IN
 				$query="SELECT * FROM Accounts WHERE username='".mysqli_real_escape_string($link, $_POST['username'])."';";
 
 				$result = mysqli_query($link, $query);
 
 				$row = mysqli_fetch_array($result);
 
+				// FOR DECRYPTING A MD5 PASSWORD, NOT CURRENTLY WORKING
 				if (array_key_exists("account_id", $row)) {
 					//$hashedPassword = md5(md5($row['Account_Id']).$_POST['password']);
 
@@ -90,6 +107,7 @@ and open the template in the editor.
 					//}
 				}
 
+				// Alternative to md5, cleartext.
 				if ($_POST['password'] == $row['password']) {
 					$_SESSION['id'] = $row['account_id'];
 
@@ -99,11 +117,15 @@ and open the template in the editor.
 				}
 			}
 		}
+		
+		mysqli_close($link);
 	}
 ?>
 
+<!-- DISPLAYS ERROR -->
 <div id="error"><?php echo $error; ?></div>
 
+<!-- INPUTS FOR ACCOUNT CREATION -->
 <form method="post">
 	<input type="username" name="username" placeholder="Username">
 
@@ -116,6 +138,7 @@ and open the template in the editor.
 	<input type="submit" name="submit" value="Create an Account">
 </form>
 
+<!-- INPUTS FOR ACCOUNT LOGIN -->
 <form method="post">
 	<input type="username" name="username" placeholder="Username">
 
